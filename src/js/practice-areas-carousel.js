6c857876ad30dbb66d8a8153fe3f2581ca438c10
Raúl Doezón <santor3medio@gmail.com>
2019-03-26 15:13:30 -0600

    #14. Change the value of margin property from 0 to 38. Change in practice-areas-carousel.js

--- src/js/practice-areas-carousel.js
+++ src/js/practice-areas-carousel.js
@@ -6,7 +6,7 @@ practiceCarousel.owlCarousel ({
   dots: true,
   navText: ['<svg xmlns="http://www.w3.org/2000/svg" width="9" height="14" viewBox="0 0 9 14"><path fill="#0b1b4e" fill-rule="evenodd" d="M6.554 7.198L.637 1.822 1.674.607l6.952 6.555-6.719 6.793-1.079-1.178z" transform="translate(4.500000, 7.000000) rotate(-180.000000) translate(-4.500000, -7.000000)"/></svg>',
   '<svg xmlns="http://www.w3.org/2000/svg" width="9" height="14" viewBox="0 0 9 14"><path fill="#0b1b4e" fill-rule="evenodd" d="M6.554 7.198L.637 1.822 1.674.607l6.952 6.555-6.719 6.793-1.079-1.178z"/></svg>'],
-  margin: 0,
+  margin: 38,
   autoWidth: true,
   center: true
 });
